fa0441b9ae45d62a686effff9335cee54279d658
BelalBasal <basdibrahim@gmail.com>
2024-07-11 21:32:47 +0300

    v01

---
+++ Assignment01OOPS06C#/Point.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment01OOPS06C_
+{
+     struct Point
+    {
+        public int x;
+        public int y;
+        public Point()
+        {
+            x = default; 
+            y = default;
+        }
+        public Point(int _x, int _y) 
+        {
+            x = _x; 
+            y= _y;
+        }
+        public override string ToString()
+        {
+            return $"({x},{y})";
+        }
+            
+    }
+}
--- Assignment01OOPS06C#/Program.cs
+++ Assignment01OOPS06C#/Program.cs
@@ -2,9 +2,22 @@
 {
     internal class Program
     {
+        struct employee
+        {
+            int id;
+            int salary;
+            string name;
+
+        }
         static void Main(string[] args)
         {
-            
+            #region V01
+            Point point1 = new Point();
+            Console.WriteLine(point1.ToString());
+            Point point2 = new Point(5,6);
+            Console.WriteLine(point2.ToString());
+
+            #endregion
         }
     }
 }
