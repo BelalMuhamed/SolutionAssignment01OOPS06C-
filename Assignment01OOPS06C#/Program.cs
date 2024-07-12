8422f8c68ead05033e39b4c630d40918b3cfdf27
BelalBasal <basdibrahim@gmail.com>
2024-07-12 13:52:47 +0300

    part02Q01

--- Assignment01OOPS06C#/Program.cs
+++ Assignment01OOPS06C#/Program.cs
@@ -53,7 +53,18 @@ namespace Assignment01OOPS06C_
             Console.WriteLine(p.ToString());
             #endregion
 
+            #region part02Q01
+            /*1.	Design and implement a Class for the employees in a company:
+            Notes:
+            •	Employee is identified by an ID, Name, security level, salary, hire date and Gender.
+            •	We need to restrict the Gender field to be only M or F [Male or Female] 
+            •	Assign the following security privileges to the employee (guest, Developer, secretary and DBA) in a form of Enum.
+            •	We want to provide the Employee Class to represent Employee data in a string Form (override ToString ()), display employee salary in a currency format. [Use String.Format() Function].
+            */
+            employee em =new employee(10,"Bilal",security.DBA,35000,new DateTime(2024-10-10),gender.male);
+            Console.WriteLine(em.ToString());
+            #endregion
         }
-        
+
     }
 }
---
+++ Assignment01OOPS06C#/employee.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment01OOPS06C_
+{
+    enum security:byte 
+    {
+        guest, Developer, secretary , DBA
+    }
+    enum gender :byte
+    {
+        male,female 
+
+    }
+    internal class employee
+    {
+        private int id;
+        private string name;
+        private security securityLevel;
+        private double salary;
+        private DateTime hiredate;
+        private gender gender;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public security SecurityLevel { get; set; }
+        public double Salary { get; set; }
+        public DateOnly HireDate { get; set; }
+        public gender Gender { get; set; }
+        public employee(int id, string name, security securityLevel, double salary, DateTime hiredate, gender gender)
+        {
+            Id = id;
+            Name = name;
+            SecurityLevel = securityLevel;
+            Salary = salary;
+            this.hiredate = hiredate;
+            Gender = gender;
+            
+        }
+        public override string ToString()
+        {
+            return string.Format("ID: {0}, Name: {1}, Gender: {2}, Security Level: {3}, Salary: {4}, Hire Date: {5}",
+            Id,
+            Name,
+            Gender,
+            SecurityLevel,
+            string.Format(CultureInfo.CurrentCulture, "{0:C}", Salary),
+            HireDate.ToString("d", CultureInfo.CurrentCulture));
+        }
+    }
+}
